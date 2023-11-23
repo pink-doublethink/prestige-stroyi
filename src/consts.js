81c0efc497648e76749e7e10910a66120e67e2ed
Alexey Ovsyannikov <soliterco@gmail.com>
2023-11-23 14:20:13 +0300

    add base components

---
+++ src/consts.js
@@ -0,0 +1,6 @@
+export const SITE_TITLE = `pysto`;
+export const SITE_DESCRIPTION = 'pysto';
+export const SITE_EMAIL = 'pysto'
+export const SITE_NAME = 'pysto';
+export const SITE_URL = "https://pysto";
+export const SITE_LANG = "ru-RU";
